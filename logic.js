5cbc331739b7807d8dfc28f3178accee8ea6ad02
Gunathi <nimashagunathilaka23@gmail.com>
2024-02-22 23:04:43 +0530

    add a loader

--- logic.js
+++ logic.js
@@ -8,5 +8,13 @@ images.forEach(image => {
 $( ".draggable" ).draggable();
 });
 
+// $(".loader").css('display', "none"); //disappear element using JS
+// $(".loader").fadeOut(1000);
+
+//logic of the loader. loader run until page finish it's loading
+$(window).on('load', function (){
+    $(".loader").fadeOut(1000)
+});
+
 
 
